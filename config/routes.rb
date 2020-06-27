6aad49e27f59ad91a70fb625e1aa4d23227ca0e9
Antonio Jackson <antoniojackson@gmail.com>
2020-06-27 12:31:09 -0600

    Handrolling merchants, merchants_items, and items paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/root', to: 'welcome#index'
 
-  resources :merchants do
-    resources :items, only: [:index]
-  end
+  get '/merchants', to: 'merchants#index'
+  get '/merchants/new', to: 'merchants#new'
+  post '/merchants', to: 'merchants#create'
+  get '/merchants/:id', to: 'merchants#show'
+  get '/merchants/:id/edit', to: 'merchants#edit'
+  patch '/merchants/:id', to: 'merchants#update'
+  delete '/merchants/:id', to: 'merchants#destroy', as: 'merchant'
+  get '/merchants/:merchant_id/items', to: 'items#index'
 
-  resources :items, only: [:index, :show] do
-    resources :reviews, only: [:new, :create]
-  end
+  get '/items', to: 'items#index', as: 'items'
+  get '/items/:id', to: 'items#show', as: 'item'
+  get '/items/:item_id/reviews/new', to: 'reviews#new', as: 'new_item_review'
+  post '/items/:item_id/reviews', to: 'reviews#create', as: 'item_reviews'
 
   resources :reviews, only: [:edit, :update, :destroy]
 
